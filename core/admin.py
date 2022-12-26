60cb6c9dbe0cc3094953f674e51c53ff27e707a4
Sheharyar <phantomwitch35@gmail.com>
2022-12-26 07:48:19 -0800

    Create a product model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,4 +10,23 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+        "original_price",
+        "discount_price",
+        "stock",
+        "is_available",
+    )
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("-updated_on", "-added_on")
+    search_fields = (
+        "name",
+        "original_price",
+    )
+    list_editable = ["discount_price", "is_available"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- core/models.py
+++ core/models.py
@@ -5,11 +5,32 @@ class Category(models.Model):
     name = models.CharField("Category Name", max_length=100, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to="categories/photos", blank=True)
+    image = models.ImageField(
+        "Category Image", upload_to="photos/categories", blank=True
+    )
 
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField("Product Name", max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+    original_price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_price = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True
+    )
+    image = models.ImageField("Product Image", upload_to="photos/products")
+    description = models.TextField(blank=True)
+    stock = models.PositiveIntegerField(default=1)
+    is_available = models.BooleanField(default=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
