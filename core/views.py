e4d6fad8c4d20458cd59201fb01f7fbb9e69e44c
Sheharyar <phantomwitch35@gmail.com>
2022-12-26 08:22:10 -0800

    Modify index page template to display products

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
+
+from .models import Product
 
 # Create your views here.
 
 
-class HomePageView(TemplateView):
-    template_name = "core/index.html"
+class HomePageView(View):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all().filter(is_available=True)[:4]
+        context = {"products": products}
+        return render(request, "core/index.html", context)
