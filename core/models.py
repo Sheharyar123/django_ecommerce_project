0bd073fc29061aa4432a03d4995aee3b166c0a6d
Sheharyar <phantomwitch35@gmail.com>
2022-12-26 07:25:01 -0800

    Add a category model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField("Category Name", max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="categories/photos", blank=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     # Local
     "user.apps.UserConfig",
+    "core.apps.CoreConfig",
 ]
 
 AUTH_USER_MODEL = "user.User"
@@ -143,6 +144,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Media Files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
